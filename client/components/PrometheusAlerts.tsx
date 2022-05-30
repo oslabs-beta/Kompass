283a65aedf1a39b915396c67affc0107c585ef6b
Connor <conmmcguire@gmail.com>
2022-05-30 17:19:09 -0500

    fixed grafana UID issue

--- client/components/PrometheusAlerts.tsx
+++ client/components/PrometheusAlerts.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 const PrometheusAlerts = (): JSX.Element => {
-  <div
-    style={{
-      background: 'black',
-      marginLeft: '190px',
-      marginTop: '-90px',
-      marginRight: '10px',
-      height: 'vh',
-      width: 'vw',
-    }}
-  >
-    <iframe
-      src='http://localhost:9090/alerts'
-      width='100%'
-      height='1500px'
-      frameBorder='0'
-    ></iframe>
-  </div>
+  return (
+    <div
+      style={{
+        background: 'black',
+        marginLeft: '190px',
+        marginTop: '-90px',
+        marginRight: '10px',
+        height: 'vh',
+        width: 'vw',
+      }}
+    >
+      <iframe
+        src='http://localhost:9090/alerts'
+        width='100%'
+        height='1500px'
+        frameBorder='0'
+      ></iframe>
+    </div>
+  )
 };
 export default PrometheusAlerts;
